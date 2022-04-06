11e2a5c2a1be6a580c3db82303b0d0e1c1ab7207
joaocout <joaopedro0801@hotmail.com>
2022-04-06 13:21:25 -0300

    Updating selectedBox logic in the Grid and Box components. Now, updating the selected box doesn't update all boxes, but only the new selected and previous selected ones.
    Style changes.
    
    TODO - Refactor Keyboard component so that it doesn't depend on the selectedBox state, avoiding unnecessary rerenders.

--- src/components/Grid/index.tsx
+++ src/components/Grid/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useCallback, useContext } from 'react';
 import { View } from 'react-native';
 
 import { styles } from './styles';
@@ -9,19 +9,29 @@ import Box from '../Box';
 const Grid: React.FC = () => {
   const { grid, selectedBox, setSelectedBox } = useContext(GridContext);
 
+  const onSelectedBoxChange = useCallback(
+    selected => {
+      setSelectedBox(selected);
+    },
+    [setSelectedBox],
+  );
+
   return (
     <View>
       {grid.map((row, rowIndex) => (
         <View key={`${rowIndex}`} style={styles.row}>
-          {row.map((box, columnIndex) => (
-            <Box
-              key={`${columnIndex}`}
-              index={rowIndex * 5 + columnIndex}
-              selectedIndex={selectedBox}
-              box={box}
-              onSelectedBoxChange={selected => setSelectedBox(selected)}
-            />
-          ))}
+          {row.map((box, columnIndex) => {
+            const index = rowIndex * 5 + columnIndex;
+            return (
+              <Box
+                key={`${columnIndex}`}
+                index={index}
+                isSelected={selectedBox === index}
+                box={box}
+                onSelectedBoxChange={onSelectedBoxChange}
+              />
+            );
+          })}
         </View>
       ))}
     </View>
